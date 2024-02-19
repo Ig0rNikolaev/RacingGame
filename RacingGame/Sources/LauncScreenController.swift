6df894e6ce726454a31c26f2d6a51a29fe71a02f
Ig0rNikolaev <igor01131987@gmail.com>
2024-02-19 18:15:34 +0300

    Added  lauching screen.
    - added launch image
    - added animation

---
+++ RacingGame/Sources/LauncScreenController.swift
@@ -0,0 +1,78 @@
+//
+//  LauncScreenController.swift
+//  RacingGame
+//
+//  Created by Игорь Николаев on 19.02.2024.
+//
+
+import UIKit
+import SnapKit
+
+fileprivate enum Constants {
+    // Images
+    static let launchImage = "lauching"
+
+    // Layouts
+    static let launchImageWidth = 300
+    static let launchImageHeight = 150
+
+    // Animations
+    static let launchImageDuration = 1.0
+    static let launchImageDelay = 0.0
+    static let launchImageAlpha = 0.0
+}
+
+final class LauncScreenController: UIViewController {
+
+    //: MARK: - UI Elements
+
+    private lazy var launchImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: Constants.launchImage)
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+
+    //: MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupAnimation()
+    }
+
+    //: MARK: - Setups
+
+    private func setupAnimation() {
+        UIView.animate(withDuration: Constants.launchImageDuration,
+                       delay: Constants.launchImageDelay,
+                       options: .curveLinear) {
+            self.launchImageView.alpha = Constants.launchImageAlpha
+        } completion: { _ in
+            // переход на стартовый экран
+        }
+    }
+
+    private func setupView() {
+        view.backgroundColor = .systemGray6
+    }
+
+    private func setupHierarchy() {
+        view.addSubview(launchImageView)
+    }
+
+    private func setupLayout() {
+        launchImageView.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.centerY.equalTo(view.snp.centerY)
+            make.width.equalTo(Constants.launchImageWidth)
+            make.height.equalTo(Constants.launchImageHeight)
+        }
+    }
+}
