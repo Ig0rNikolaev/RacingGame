0fed6518adbad500e97671ddda4cfa88c95e4631
Ig0rNikolaev <igor01131987@gmail.com>
2024-03-27 12:33:56 +0300

    Refact AppDelegete.

--- RacingGame/Sources/App/AppDelegate.swift
+++ RacingGame/Sources/App/AppDelegate.swift
@@ -14,13 +14,16 @@ private extension String {
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, 
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_ application: UIApplication, 
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: .defaultConfiguration, sessionRole: connectingSceneSession.role)
     }
 
